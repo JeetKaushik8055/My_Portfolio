c7d9e3635ab70b623c544ced5db7384f06b8f9f4
Jeet Kaushik <jeet.kaushik2020@vitstudent.ac.in>
2021-08-31 11:06:39 +0530

    Till Project's section completed

---
+++ Js/Brain.js
@@ -0,0 +1,13 @@
+var text = ["Jeet Kaushik", "जीत कौशिक", "جیت کوشک۔","杰特考希克"];
+var counter = 0;
+var elem = document.getElementById("heading-text"); //heading id
+var inst = setInterval(change, 800);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
